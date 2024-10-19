862de5bf8d919f447b327d700e94484c56a5b9c5
Felipe Panosso <149212013+FelipePn10@users.noreply.github.com>
2024-10-18 23:03:19 -0300

    v0.1.5

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useToast } from "@/hooks/use-toast";
-import { ChevronDown, Loader2 } from "lucide-react";
+import { ChevronDown, ChevronUp, Loader2 } from "lucide-react";
 import { Document, Page, pdfjs } from "react-pdf"
 
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css'
@@ -10,10 +10,14 @@ import 'react-pdf/dist/esm/Page/TextLayer.css'
 import { useResizeDetector } from "react-resize-detector"
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
+import { useState } from "react";
 
+import { useForm } from "react-hook-form"
+import { z } from "zod";
 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { cn } from "@/lib/utils";
 
-//import { pdfjs } from 'react-pdf';
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
     'pdfjs-dist/build/pdf.worker.min.mjs',
@@ -29,19 +33,76 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
 
     const { toast } = useToast()
 
+    const [numPages, setNumPages] = useState<number>()
+    const [currPage, setCurrPage] = useState<number>(1)
+
+    const CustomPageValidator = z.object({
+        page: z.string().refine((num) => Number(num) > 0 && Number(num) <= numPages!)
+    })
+
+    type TCustomPageValidator = z.infer<typeof CustomPageValidator>
+
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        setValue
+    } = useForm<TCustomPageValidator>({
+        defaultValues: {
+            page: "1"
+        },
+        resolver: zodResolver(CustomPageValidator)
+    })
+
     const { width, ref } = useResizeDetector()
 
+    const handlePageSubmit = ({
+        page }: TCustomPageValidator) => {
+        setCurrPage(Number(page))
+        setValue("page", String(page))
+    }
+
     return (
-        <div className="w-full bg-white rounded-md shadow flex flex-col itemns-center">
+        <div className="w-full bg-white rounded-md shadow flex flex-col itemns-center" >
             <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
                 <div className="flex itemns-center gap-1.5">
-                    <Button variant="ghost" aria-label="previous page">
+                    <Button
+                        disabled={currPage <= 1}
+                        onClick={() => {
+                            setCurrPage((prev) => (prev - 1 > 1 ? prev - 1 : 1)
+                            )
+                        }}
+                        variant="ghost"
+                        aria-label="previous page">
                         <ChevronDown className="h-4 w-4" />
                     </Button>
 
                     <div className="flex items-center gap-1.5">
-                        <Input className="w-12 h-8" />
+                        <Input
+                            {...register("page")}
+                            className={cn('w-12 h-8', errors.page && "focus-visible:ring-red-500")}
+                            onKeyDown={(e) => {
+                                if (e.key === "Enter") {
+                                    handleSubmit(handlePageSubmit)()
+                                }
+                            }}
+                        />
+                        <p className="text-zinc-700 text-xm space-x-1">
+                            <span>/</span>
+                            <span>{numPages ?? "x"}</span>
+                        </p>
                     </div>
+
+                    <Button
+                        disabled={numPages === undefined || currPage === numPages}
+                        onClick={() => {
+                            setCurrPage((prev) => prev + 1 > numPages! ? numPages! : prev + 1)
+                        }}
+                        variant="ghost"
+                        aria-label="next page">
+                        <ChevronUp className="h-4 w-4" />
+                    </Button>
                 </div>
             </div>
 
@@ -60,12 +121,16 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
                                 variant: 'destructive'
                             })
                         }}
+                        onLoadSuccess={({ numPages }) => setNumPages(numPages)}
                         file={url} className="max-h-full">
-                        <Page width={width ? width : 1} pageNumber={1} />
+                        <Page
+                            width={width ? width : 1}
+                            pageNumber={currPage}
+                        />
                     </Document>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
