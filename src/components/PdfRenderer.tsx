57bd33454a9ebbbd014e499ef3171251a8a1ea32
Felipe Panosso <149212013+FelipePn10@users.noreply.github.com>
2024-10-19 14:03:38 -0300

    v0.1.8

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -46,6 +46,9 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
     const [currPage, setCurrPage] = useState<number>(1)
     const [scale, setScale] = useState<number>(1)
     const [rotation, setRotation] = useState<number>(0)
+    const [renderedScale, setRenderedScale] = useState<number | null>(null)
+
+    const isLoading = renderedScale !== scale
 
     const CustomPageValidator = z.object({
         page: z.string().refine((num) => Number(num) > 0 && Number(num) <= numPages!)
@@ -182,11 +185,27 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
                             }}
                             onLoadSuccess={({ numPages }) => setNumPages(numPages)}
                             file={url} className="max-h-full">
-                            <Page
+                            {isLoading && renderedScale ? <Page
                                 width={width ? width : 1}
                                 pageNumber={currPage}
                                 scale={scale}
                                 rotate={rotation}
+                                key={"@" + renderedScale}
+                            /> : null}
+
+                            <Page
+                                className={cn(isLoading ? "hidden" : "")}
+                                width={width ? width : 1}
+                                pageNumber={currPage}
+                                scale={scale}
+                                rotate={rotation}
+                                key={"@" + scale}
+                                loading={
+                                    <div className="flex justify-center">
+                                        <Loader2 my-24 h-6 w-6 animate-spin />
+                                    </div>
+                                }
+                                onRenderSuccess={() => setRenderedScale(scale)}
                             />
                         </Document>
                     </div>
